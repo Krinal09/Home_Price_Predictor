4560e18d841942566c3f35d37789914374d9e96e
Krinal09 <104003095+Krinal09@users.noreply.github.com>
2024-06-01 23:09:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, request
+from flask import Flask, render_template, request
 import pandas as pd
 import numpy as np
 import pickle
@@ -6,37 +6,26 @@ import pickle
 app = Flask(__name__)
 
 data = pd.read_csv('Clean_Data.csv')
-pipe = pickle.load(open("Model.pkl",'rb'))
+pipe = pickle.load(open("Model.pkl", 'rb'))
 
 @app.route('/')
 def index():
-    
     locations = sorted(data['location'].unique())
-    return render_template('index.html', locations = locations)
+    return render_template('index.html', locations=locations)
 
-
-@app.route('/predict', methods=['POST'] )
+@app.route('/predict', methods=['POST'])
 def predict():
-    
-    location=request.form.get('location')
+    location = request.form.get('location')
+    bhk = request.form.get('bhk')
+    bath = request.form.get('bath')
+    sqft = request.form.get('total_sqft')
 
-    bhk=request.form.get('bhk')
-    
-    bath=request.form.get('bath')
-    
-    sqft=request.form.get('total_sqft')
-    
-    print(location, bhk, bath, sqft)
+    input_data = pd.DataFrame([[location, sqft, bath, bhk]], columns=['location', 'total_sqft', 'bath', 'bhk'])
+    prediction = pipe.predict(input_data)[0] + 1e5
 
-    input = pd.DataFrame( [[location,sqft,bath,bhk]], columns=['location', 'total_sqft', 'bath', 'bhk'] )
-                    
-    prediction = pipe.predict(input)[0] + 1e5
-              
-    # print(prediction)
-
-    return str(np.round(prediction,2))
-
+    return str(np.round(prediction, 2))
 
 if __name__ == "__main__":
-    app.run(debug = True, port=5002)
-    
+    # Use the PORT environment variable provided by Render
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
