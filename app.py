a2fe8d80ea8736d973de6d291b79438b286f3cfb
Krinal09 <104003095+Krinal09@users.noreply.github.com>
2024-06-01 23:47:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,4 @@ def predict():
     return str(np.round(prediction, 2))
 
 if __name__ == "__main__":
-    # Use the PORT environment variable provided by Render
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True)
